d9622a56180c5d91bbba40932ede2cc281f44481
wannarat yuangprasit <mickeypkg06@gmail.com>
2025-11-03 14:59:20 +0700

    Fix: API Change

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,7 +1,7 @@
 //
 // --- API Base URL ---
 //
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://chinsanparty-backend.onrender.com";
 //
 // --- Language Dictionary ---
 const translations = {
